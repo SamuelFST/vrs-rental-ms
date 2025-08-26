65d8d23093569f093d57f633f80a013d38744de6
SamuelFST <guti.samuel2002@gmail.com>
2025-08-25 22:59:45 -0300

    create base project

---
+++ src/main/java/vrs/rental_ms/RentalMsApplication.java
@@ -0,0 +1,13 @@
+package vrs.rental_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalMsApplication.class, args);
+	}
+
+}
